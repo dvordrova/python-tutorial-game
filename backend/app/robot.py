635c1a5a817c97df93453e99ade96c6dfa55cf43
Dmitry Bozhko <meridos.programs@yandex.ru>
2024-02-20 22:32:56 +0400

    make velocity hashable

--- backend/app/robot.py
+++ backend/app/robot.py
@@ -3,7 +3,7 @@ from typing import Dict, Tuple
 from dataclasses import dataclass, field
 from uuid import uuid4, UUID
 
-@dataclass
+@dataclass(frozen=True)
 class Velocity:
     x: int
     y: int
@@ -34,20 +34,20 @@ class Robot:
     sensors: Dict = field(default_factory=dict)
 
     uuid: UUID = field(default_factory=uuid4, repr=False)
-    
+
     def make_step(self, step: int):
         if self.movable:
             self.prev_x = self.x
             self.prev_y = self.y
             self.x += self.velocity.x
             self.y += self.velocity.y
-    
+
     def make_step_back(self):
         self.x = self.prev_x
         self.y = self.prev_y
-    
+
     def collides(self, other: 'Robot'):
         return self.uuid != other.uuid and self.x == other.x and self.y == other.y
-    
+
     def __eq__(self, other: Tuple[int, int]):
         return self.x == other[0] and self.y == other[1]
