78714d34ee3e3df265ca030541ffebe1e54dbe2d
Dmitry Bozhko <meridos.programs@yandex.ru>
2026-08-25 22:53:58 +0400

    fix: comething

--- backend/main.py
+++ backend/main.py
@@ -6,6 +6,11 @@ import uvicorn
 from app.settings import settings
 
 
+class App:
+    @property
+    def app(self):
+        from app.app import app
+        return app
 if __name__ == '__main__':
     uvicorn.run(
         'app.app:app',
